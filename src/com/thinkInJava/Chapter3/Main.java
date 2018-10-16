8f1d5c7420675e001da03ba14c85406b001e9100
weyrue <weyrue@gmail.com>
2018-10-16 19:30:09 +0800

    10/16/18 Update

--- src/com/thinkInJava/Chapter3/Main.java
+++ src/com/thinkInJava/Chapter3/Main.java
@@ -3,7 +3,6 @@ package com.thinkInJava.Chapter3;
 import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) {
 
     }
--- src/com/thinkInJava/Chapter5/NewVarArgs.java
+++ src/com/thinkInJava/Chapter5/NewVarArgs.java
@@ -2,7 +2,7 @@ package com.thinkInJava.Chapter5;
 
 public class NewVarArgs {
     public static void main(String[] args) {
-        printArray(new Integer(47), new Float(3.14), new Double(11.11));
+        printArray(47, 3.14, 11.11);
         printArray("one", "two", "three");
         printArray(new A(), new A(), new A());
         printArray((Object[]) new Integer[]{1, 2, 3, 4});
