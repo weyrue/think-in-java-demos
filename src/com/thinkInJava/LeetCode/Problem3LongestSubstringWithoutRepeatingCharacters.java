9d260404077a7215a36da97043800f32bc67f7ba
zy <weyrue@gmail.com>
2018-11-23 18:16:13 +0800

    11/23/18 Update-3

--- src/com/thinkInJava/LeetCode/Problem3LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/thinkInJava/LeetCode/Problem3LongestSubstringWithoutRepeatingCharacters.java
@@ -6,15 +6,16 @@ import java.util.Map;
 public class Problem3LongestSubstringWithoutRepeatingCharacters {
 
     public static void main(String[] args) {
-        String s = "tmmzuxtm";
+        String s = "bbbb";
         System.out.println("maxLength: " + lengthOfLongestSubstring(s));
     }
 
     public static int lengthOfLongestSubstring(String s) {
+        int length = s.length();
         int startPos = 0;
         int maxLength = 0;
         Map<Character, Integer> characterIntegerMap = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < length - 1; i++) {
             char c = s.charAt(i);
             if (characterIntegerMap.containsKey(c) && characterIntegerMap.get(c) >= startPos) {
                 int tempLength = i - startPos;
@@ -22,16 +23,27 @@ public class Problem3LongestSubstringWithoutRepeatingCharacters {
                     maxLength = tempLength;
                 }
                 startPos = characterIntegerMap.get(c) + 1;
-                characterIntegerMap.put(c, i);
-            } else if (i == s.length() - 1) {
-                int tempLength = i - startPos + 1;
+            }
+            characterIntegerMap.put(c, i);
+        }
+
+        // 循环至队尾的情况单独考虑
+        if (length > 0) {
+            int i = length - 1;
+            char c = s.charAt(i);
+            if (characterIntegerMap.containsKey(c) && characterIntegerMap.get(c) >= startPos) {
+                int tempLength = i - startPos;
                 if (tempLength > maxLength) {
                     maxLength = tempLength;
                 }
             } else {
-                characterIntegerMap.put(c, i);
+                int tempLength = i - startPos + 1;
+                if (tempLength > maxLength) {
+                    maxLength = tempLength;
+                }
             }
         }
+
         return maxLength;
     }
 }
