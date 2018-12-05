f36079c3c0ab29fa3a8024d5d355304591f19f4a
Yi <weyrue@outlook.com>
2018-12-05 22:39:23 +0800

    12/5/18 Update

--- src/com/thinkInJava/Chapter18/GetChannel.java
+++ src/com/thinkInJava/Chapter18/GetChannel.java
@@ -5,27 +5,29 @@ import java.io.FileOutputStream;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 
 public class GetChannel {
     private static final int BSIZE = 1024;
 
     public static void main(String[] args) throws Exception {
-        FileChannel fc = new FileOutputStream("data.txt").getChannel();
+        FileChannel fc = new FileOutputStream("./src/com/thinkInJava/Chapter18/data.txt").getChannel();
 
         fc.write(ByteBuffer.wrap("Some text ".getBytes()));
         fc.close();
 
-        fc = new RandomAccessFile("data.txt", "rw").getChannel();
+        fc = new RandomAccessFile("./src/com/thinkInJava/Chapter18/data.txt", "rw").getChannel();
         fc.position(fc.size());
         fc.write(ByteBuffer.wrap("Some more".getBytes()));
         fc.close();
 
-        fc = new FileInputStream("data.txt").getChannel();
+        fc = new FileInputStream("./src/com/thinkInJava/Chapter18/data.txt").getChannel();
         ByteBuffer buff = ByteBuffer.allocate(BSIZE);
         fc.read(buff);
         buff.flip();
 
-        while (buff.hasRemaining())
-            System.out.print((char) buff.get());
+        System.out.println(Charset.forName(System.getProperty("file.encoding")).decode(buff));
+//        while (buff.hasRemaining())
+//            System.out.print((char) buff.get());
     }
 }
---
+++ src/com/thinkInJava/Chapter18/GetData.java
@@ -0,0 +1,33 @@
+package com.thinkInJava.Chapter18;
+
+import com.thinkInJava.Chapter6.Print;
+
+import java.nio.ByteBuffer;
+import java.util.BitSet;
+
+import static com.thinkInJava.Chapter6.Print.print;
+
+public class GetData {
+    private static final int BSIZE = 1024;
+
+    public static void main(String[] args) {
+        ByteBuffer bb = ByteBuffer.allocate(BSIZE);
+
+        int i=0;
+        while (i++ < bb.limit()) {
+            if (bb.get() != 0) {
+                print("nonzero");
+            }
+        }
+        print("i = "+i);
+        bb.rewind();
+
+        bb.asIntBuffer().put(99471124);
+        print(bb.getInt());
+        bb.rewind();
+
+        bb.asFloatBuffer().put(99471124);
+        print(bb.getFloat());
+        bb.rewind();
+    }
+}
